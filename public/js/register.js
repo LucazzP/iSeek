47e61f74e79a725627c067ce6ac1ef68ce56e7a3
Renan Maciel <rmaciel42@gmail.com>
2019-05-28 17:22:09 -0300

    terminando cadastro

--- public/js/register.js
+++ public/js/register.js
@@ -4,9 +4,12 @@ var emailRegister = document.getElementById('InputEmail');
 var passwordRegister = document.getElementById('InputPassword');
 var passwordRegister2 = document.getElementById('RepeatPassword');
 var btnRegi = document.getElementById('registerBtn');
+var firestore = firebase.firestore();
+var setDoc
 
-
+//Adicionando usuario e senha
 btnRegi.addEventListener('click', function() {
+    //Usuario e senha
     firebase.auth().createUserWithEmailAndPassword(emailRegister.value, passwordRegister.value).then(function(result){
         alert("Usuario Conectado!")
         console.log("Sucess")
@@ -22,6 +25,17 @@ btnRegi.addEventListener('click', function() {
         alert("Codigo de erro " + errorCode);
         console.log("error")
       });
-
+    //Enviando ao firestoreDB os dados do usuario
+    setDoc = firestore.collection("users").doc().set({
+        nome: nameRegister.value,
+        sobrenome: lastNameRegister.value,
+        email: emailRegister.value
+    })
+    .then(function(){
+        console.log("arquivos enviandos com sucesso")
+    })
+    .catch(function(error){
+        console.log("erro", error)
+    });
 
 });
