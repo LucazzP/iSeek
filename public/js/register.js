850c4bd27e5f49910ce68ece5a5742bb431c4a2f
Renan Maciel <rmaciel42@gmail.com>
2019-05-28 08:25:49 -0300

    login com usuario e senha prontos

--- public/js/register.js
+++ public/js/register.js
@@ -2,30 +2,26 @@ var nameRegister = document.getElementById('FirstName');
 var lastNameRegister = document.getElementById('LastName');
 var emailRegister = document.getElementById('InputEmail');
 var passwordRegister = document.getElementById('InputPassword');
-//var passwordRegister2 = document.getElementById('RepeatPassword');
+var passwordRegister2 = document.getElementById('RepeatPassword');
 var btnRegi = document.getElementById('registerBtn');
 
 
-//Ao clicar no botao
+btnRegi.addEventListener('click', function() {
+    firebase.auth().createUserWithEmailAndPassword(emailRegister.value, passwordRegister.value).then(function(result){
+        alert("Usuario Conectado!")
+        console.log("Sucess")
+        window.location.replace('login.html');
+        
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
 
-btnRegi.addEventListener('click', function(){
-    create(nameRegister.value, 
-            lastNameRegister.value, 
-            emailRegister.value,
-            passwordRegister.value,);
+        alert(errorMessage);
+        alert("Codigo de erro " + errorCode);
+        console.log("error")
+      });
+
+
 });
-
-
-
-//Empacotando dados para enviar ao firebase
-function create(name, lastname, email, password){
-    var data = {
-        name: name,
-        last:lastname,
-        email: email,
-        password: password
-    };
-
-    return firebase.database().ref().child('users').push(data);
-}
-
