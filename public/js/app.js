de1ecf113fa16e915118f23401f65c43dec25ea1
LucazzP <lucas.polazzo@executive.com.br>
2019-06-16 19:51:47 -0300

    Começando modularização

--- public/js/app.js
+++ public/js/app.js
@@ -12,19 +12,49 @@ var firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
+var isProfessor;
+var navHTML = '<!-- Divider -->'+
+                '<hr class="sidebar-divider">'+
+    
+                '<!-- Heading -->'+
+                '<div class="sidebar-heading">'+
+                    'Gerir projetos'+
+                '</div>'+
+                
+                '<!-- Nav Item - Novo projeto -->'+
+                '<li class="nav-item active" id="newProjectNav">'+
+                '</li>'+
+    
+                '<!-- Nav Item - Meus projetos -->'+
+                '<li class="nav-item">'+
+                    '<a class="nav-link" href="#" id="navMyProjects">'+
+                    '<i class="fas fa-fw fa-table"></i>'+
+                    '<span>Meus projetos</span></a>'+
+                '</li>'+
+    
+                '<!-- Nav Item - Novo projeto -->'+
+                '<li class="nav-item">'+
+                    '<a class="nav-link" href="#" id="navChat">'+
+                    '<i class="fas fa-fw fa-chart-area"></i>'+
+                    '<span>Chat</span></a>'+
+                '</li>';
+
 function iniciar(){
+    $.
+
     firebase.auth().onAuthStateChanged(function(authData){
         var page = window.location.pathname.split('/');
         page = page[page.length - 1];
     
         if(page == 'index.html' || page == 'register.html'){
-            if(authData != null){
+            if(authData != null && page == 'index.html'){
                 window.location.assign('plataform.html');
             }
         } else {
             if(authData == null){
                 window.location.assign('index.html');
             }
+            checkIfIsProfessor(authData.uid);
             getUsernameAndSubstitute();
             try {
                 var btnSair = document.getElementById('btn-sair');
@@ -37,19 +67,33 @@ function iniciar(){
     });
 }
 
-function verifyFire(){
-    if(firebase.auth().currentUser != null){
-        
-    } else {
-        verifyFire();
-    }
-}
-
 function getUsernameAndSubstitute(){
     var username = document.getElementById('username');
     var uid = firebase.auth().currentUser.uid;
     firebase.firestore().collection('users').doc(uid).get().then(function(doc){
         username.innerText = doc.data()['nome'] + ' ' + doc.data()['sobrenome'];
+    });
+}
+
+function checkIfIsProfessor(uid){
+    firebase.firestore().collection('users').doc(uid).get().then(function(data){
+        if(data.data()['typeUser'] == 'souAluno'){
+            isProfessor = false;
+        } else {
+            var newProjectNav = document.getElementById('newProjectNav');
+            newProjectNav.innerHTML = '<a class="nav-link" href="new-project.html"> <i class="fas fa-fw fa-chart-area"></i> <span>Novo projeto</span></a>';
+            isProfessor = true;
+        }
+    });
+}
+
+function checkIfHaveProjects(uid){
+    firebase.firestore().collection('projetos').where('id-alunos', "array-contains", uid).get().then(function(data){
+        var nav = document.getElementById('gerirProjectosNav');
+        if(!data.empty){
+            nav.innerHTML = navHTML;
+            return true;
+        }
     });
 }
 
