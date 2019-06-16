3e59d9dc5bc96bbc9664cf241dae1cd5715f938b
LucazzP <lucas.polazzo@executive.com.br>
2019-06-16 14:20:38 -0300

    New project fucnionando

--- public/js/new-project.js
+++ public/js/new-project.js
@@ -9,6 +9,8 @@ var aptidaoCursoBtn = document.getElementById('aptidaoCursoBtn');
 var nomeProjeto = document.getElementById('nomeProjeto');
 var vagas = document.getElementById('vagas');
 var registerBtn = document.getElementById('registerBtn');
+var descricaoProjeto = document.getElementById('descricaoProjeto');
+var username = document.getElementById('username');
 
 dropProjetos.addEventListener('click', function(value){
     dropProjetosButton.innerHTML = value.target.innerHTML;
@@ -37,14 +39,17 @@ registerBtn.addEventListener('click', function(){
                         });
                         if(cursos.length != 0){
                             firebase.firestore().collection('projetos').add({
-                                "bolsa": temBolsa.innerText,
+                                "bolsa": temBolsaBtn.innerText,
                                 "curso-necessario": cursos,
-                                "description" : "",
-                                "duracao": duracao.innerText,
+                                "description": descricaoProjeto.value,
+                                "duracao": duracaoBtn.innerText,
                                 "id-alunos": [],
-                                "id-professor": ""
-                            }
-                            );
+                                "id-professor": firebase.auth().currentUser.uid,
+                                "nome-professor": username.innerText,
+                                "nome-projeto": nomeProjeto.value,
+                                "tipo": dropProjetosButton.innerText,
+                                "vagas": vagas.value,
+                            });
                         } else alert("Selecione os cursos requisitatos para a vaga!");
                     } else alert("Selecione se seu projeto possui bolsa!");
                 } else alert("Digite a quantidades de vagas para seu projeto!");
