a2fd7dcedb2bfdea70ca99c3fd30c8ad6ca9891b
pdriNho <pedrolucho02@gmail.com>
2019-06-16 23:26:51 -0300

    Modal Convidar Participantes

--- public/js/my-project.js
+++ public/js/my-project.js
@@ -17,7 +17,7 @@ function loadPage(id) {
         data = doc.data();
         projectName = data['nome-projeto'];
 
-        if(doc.data()['id-alunos'] != []){
+        if (doc.data()['id-alunos'] != []) {
             participantName = doc.data()['nome-alunos'][0];
         } else participantName = 'Não há alunos!';
 
@@ -225,12 +225,38 @@ function updateEmpthyFields() {
     });
 }
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 addCard(0);
 addCard(1);
 addCard(2);
 addCard(3);
 
-firebase.auth().onAuthStateChanged(function(authData){
+firebase.auth().onAuthStateChanged(function(authData) {
     loadPage(id);
 });
 
